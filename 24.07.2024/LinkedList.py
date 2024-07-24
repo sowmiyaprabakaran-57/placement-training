2fa798d76078bd25bcf9fd8965850767be86cda3
sowmiyaprabakaran-57 <168167244+sowmiyaprabakaran-57@users.noreply.github.com>
2024-07-24 21:45:41 +0530

    Create LinkedList.py

---
+++ 24.07.2024/LinkedList.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def insert_at_beginning(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" ")
+            temp = temp.next
+llist = LinkedList()
+llist.insert_at_beginning(1)
+llist.insert_at_beginning(2)
+llist.insert_at_beginning(3)
+llist.print_list()
